0ad17d28aaf710e4c11c24da692b07d25d10bf8e
Etnojius <81381496+Etnojius@users.noreply.github.com>
2024-12-01 17:43:55 +0100

    Multiplayer working

--- Assets/Scripts/Multiplayer/PrivateNetworkManager.cs
+++ Assets/Scripts/Multiplayer/PrivateNetworkManager.cs
@@ -2,10 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using TMPro;
 
 public class PrivateNetworkManager : MonoBehaviour
 {
     private NetworkManager networkManager;
+
+    [SerializeField]
+    private TMP_Text joinCodeOutput;
+    [SerializeField]
+    private TMP_InputField joinCodeInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +39,42 @@ public class PrivateNetworkManager : MonoBehaviour
     public void StartClient()
     {
         networkManager.StartClient();
+    }
+
+    public async void Host()
+    {
+        string joinCode = await StartHostWithRelay();
+        joinCodeOutput.text = joinCode;
+    }
+
+    public async void Join()
+    {
+        await StartClientWithRelay(joinCodeInput.text);
+    }
+
+    public async Task<string> StartHostWithRelay(int maxConnections = 5)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        return NetworkManager.Singleton.StartHost() ? joinCode : null;
+    }
+
+    public async Task<bool> StartClientWithRelay(string joinCode)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
     }
 }
